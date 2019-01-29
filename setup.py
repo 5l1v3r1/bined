2deddaf83a0fd03157297643fd27173e9415b808
OseidAldary(Joker11) <oseid.aldary@gmail.com>
2019-01-29 17:10:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
-#!/usr/bin/env python
-
 from setuptools import find_packages, setup
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
-
 setup(name='bined',
       version='1.0',
       url='https://github.com/Oseid/bined',
-      download_url="https://github.com/Oseid/bined/tarball/master",
       author='oseid aldary',
       author_email='oseid.eng@gmail.com',
       description='encode/decode binary',
-      keywords='Encode, Decode, Binary',
+      keywords='bin, Encode, Decode, Binary',
       packages=find_packages(),
       py_modules=['bined'],
       data_files=[('', ['LICENSE'])],
@@ -22,13 +17,7 @@ setup(name='bined',
         'Intended Audience :: Developers',
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Operating System :: OS Independent',
         'Environment :: Console',
